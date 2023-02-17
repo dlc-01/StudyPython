d685bae7f131d653a3c261b03aa76018961d4290
DLC <55656405+dlc-01@users.noreply.github.com>
2023-02-17 21:11:55 +0300

    Create SecondPlayer.java

---
+++ Java/Beginers/battleship/SecondPlayer.java
@@ -0,0 +1,10 @@
+package battleship;
+
+
+class secondPlayer extends firstPlayer {
+    public static String[][] FieldPLayerTwo = new String[10][10];
+    public static String[][] HidenFieldTwo = new String[10][10];
+    public static String[][] PseodoFieldTwo = new String[10][10];
+    public static int CounterHitShipTwo = 17;
+
+}
